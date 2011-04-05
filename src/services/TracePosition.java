c515ed92abccaa574f922b51a9b4753686de393b
teeph <teeph@wanadoo.fr>
2011-04-05 10:13:46 +0200

    suite

---
+++ src/services/TracePosition.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package services;
+
+import android.app.IntentService;
+
+
+/**
+ *
+ * @author TeepH
+ */
+public class TracePosition extends IntentService {
+  public TracePosition(){
+      
+  }
+}
